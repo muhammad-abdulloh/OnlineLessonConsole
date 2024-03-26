9f1919b7527ea3ba13f6622775f2e67ed84f4952
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-03-26 18:07:20 +0500

    delegatelar

--- MyCollections/Program.cs
+++ MyCollections/Program.cs
@@ -1,152 +1,57 @@
-﻿using MyCollections.Application.Services;
-using MyCollections.Models;
-using System.Collections;
-
-namespace MyCollections
+﻿namespace MyCollections
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public delegate void MyDelegate(int x);       
+
+        public static void Main(string[] args)
         {
 
-            /*
-            PatientService patientService = new PatientService();
-
-            IEnumerable<Patient> result =  patientService.GetPatients();
-
-            foreach (Patient patient in result)
+            // native delegate with anonym method
+            MyDelegate del2 = delegate (int z)
             {
-                Console.WriteLine($"Id: {patient.Id}, Name: {patient.Name}, SSN: {patient.SSN}");
-            }
-
-            //1
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            Patient patient1 = new Patient()
-            {
-                Id = 8,
-                Name = "Test",
-                SSN = 123123
+                // logic section
+                Console.WriteLine("Delegate ishlavotti");
+                Console.WriteLine("davay davay");
             };
 
-            IEnumerable<Patient> result2 = patientService.CreatePatient(patient1);
+            del2(45);
 
-
-            
-
-            foreach (Patient patient in result2)
+            // func with anonym method
+            Func<int, double> myDel = delegate (int number)
             {
-                Console.WriteLine($"Id: {patient.Id}, Name: {patient.Name}, SSN: {patient.SSN}");
-            }
-
-            Console.ForegroundColor= ConsoleColor.Blue;
-
-
-            ICollection<Patient> result3 = (ICollection<Patient>)patientService.GetPatients();
-
-            // SSN raqami ga teng elementni topish kere
-            // va barcha malumotlarni chiqarish kere
-
-            Patient value = patientService.GetPatientBySSN(123123);
-
-            Console.WriteLine($"Id: {value.Id}, Name: {value.Name}, SSN: {value.SSN}");
-           
-            Console.ForegroundColor = ConsoleColor.White;
-
-
-
-
-
-
-
-            ICollection<int> collection = new List<int>();
-
-            IList<int> list2 = new List<int>();
-
-            ArrayList array = new ArrayList()
-            {
-                23, 32, 3,123,12 ,3,12,3
+                Console.WriteLine("Func sihlavotti");
+                return number * number;
             };
 
-            IDictionary dictionary = new Hashtable();
+            double value = myDel(5);
+            Console.WriteLine(value);
 
-            */
-            
-            PatientService service = new PatientService();
-
-            service.GetPatients();
-
-            var result = service.DeletePatient(3);
-
-
-            foreach (var item in result)
+            // Action with Anonym method
+            Action<string, int> myAct = delegate (string x, int z)
             {
-                Console.WriteLine(item.Id);
-            }
-            
-
-
-
-
-            /**
-            List<Patient> patients2 = new List<Patient>()
-            {
-                new Patient() {Id = 1, Name = "Shavkatjon", SSN = 12312312 },
-                new Patient() {Id = 2, Name = "Shuxratbek", SSN = 656562312 },
-                new Patient() {Id = 3, Name = "Mirkozim", SSN = 43542312 },
-                new Patient() {Id = 4, Name = "Akramjon", SSN = 23412312 },
-                new Patient() {Id = 5, Name = "Sardor", SSN = 23127689 },
-                new Patient() {Id = 6, Name = "Muxlisa", SSN = 67872312 },
-                new Patient() {Id = 7, Name = "Alisa", SSN = 563752 },
+                Console.WriteLine("Action Ishlavotti");
+                Console.WriteLine(x + " " + z);
             };
 
-            patients2.Remove(new Patient()
+            myAct("Imtihonga shuncha bor kun ", 4);
+
+            // Predicate with anonym method
+            Predicate<int> myPredicate = delegate (int number)
             {
-                Id = 1,
-                Name = "Shavkatjon",
-                SSN = 12312312
-            });
-
-            foreach (Patient pat in patients2)
-            {
-                Console.WriteLine(pat.Name);
-            }
-            */
-
-
-            /**
-            object a = "Item";
-            object b = "Item";
-
-            Console.WriteLine(a == b);
-            Console.WriteLine(a.Equals(b));
-
-            object c = "Item";
-            object d = new string("Item");
-
-            Console.WriteLine(c == d);
-            Console.WriteLine(c.Equals(d));
-
-
-
-
-            var res1 = new Patient()
-            {
-                Id = 1,
-                Name = "Shavkatjon",
-                SSN = 12312312
+                Console.WriteLine("Predicate ishlavotti");
+                return true;
             };
 
-            var res2 = new Patient()
-            {
-                Id = 1,
-                Name = "Shavkatjon",
-                SSN = 12312312
-            };
+            Console.WriteLine(myPredicate.Invoke(3));
 
+        }
 
-            Console.WriteLine(res1 == res2);
-            */
+        public static bool IsOdd(int x)
+        {
+            // Trusy, Falsy
+            // 0, null, undefined
+            return x % 2 == 1 ? true : false;
         }
     }
 }
--- OnlineLessonConsole.Models/Triangle.cs
+++ OnlineLessonConsole.Models/Triangle.cs
@@ -7,44 +7,5 @@ using System.Threading.Tasks;
 
 namespace OnlineLessonConsole.Models
 {
-    public class Triangle : Shape
-    {
-        public int Perimeter()
-        {
-          
-            return MyProperty;
-        }
-    }
-
-    public class Rectangle : Animal
-    {
-        public override string Eat()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string EyesColor()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int LegsCount()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string Name()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Perimeter()
-        {
-            Shape shape = new Shape();
-
-            shape.MyProperty = 23432;
-
-            return shape.MyProperty;
-        }
-    }
+    
 }
